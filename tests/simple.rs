40e37d41515d6fb73bbd0ea4bfeb1f544896ff5b
Michael Killough <michaeljkillough@gmail.com>
2018-06-13 23:24:03 +0100

    Split into multiple tests.

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,7 +1,6 @@
 #[macro_use]
 extern crate factori;
 
-#[derive(Debug, Eq, PartialEq)]
 struct Vehicle {
     number_wheels: u8,
     electric: bool,
@@ -23,22 +22,35 @@ factori!(Vehicle, {
 });
 
 #[test]
-fn simple_struct_with_accessible_fields() {
+fn simple_struct() {
     let default = create!(Vehicle);
     assert_eq!(default.number_wheels, 4);
     assert_eq!(default.electric, false);
+}
 
+#[test]
+fn override_field() {
     let three_wheels = create!(Vehicle, number_wheels: 3);
     assert_eq!(three_wheels.number_wheels, 3);
+}
 
+#[test]
+fn one_trait() {
     let bike = create!(Vehicle, :bike);
     assert_eq!(bike.number_wheels, 2);
     assert_eq!(bike.electric, false);
+}
 
+#[test]
+fn trait_and_override() {
     let electric_bike = create!(Vehicle, :bike, electric: true);
     assert_eq!(electric_bike.number_wheels, 2);
     assert_eq!(electric_bike.electric, true);
+}
 
-    let another_eletric_bike = create!(Vehicle, :bike, :electric);
-    assert_eq!(electric_bike, another_eletric_bike);
+#[test]
+fn two_traits() {
+    let electric_bike = create!(Vehicle, :bike, :electric);
+    assert_eq!(electric_bike.number_wheels, 2);
+    assert_eq!(electric_bike.electric, true);
 }
