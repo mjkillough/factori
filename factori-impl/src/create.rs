b9befc9db6eed7467705833dce3a4eace449764c
Michael Killough <michaeljkillough@gmail.com>
2020-08-14 18:34:02 +0100

    Remove proc-macro-rules for create!()
    This allows us to remove the dependency on proc-macro-rules, as it isn't
    actively maintained. It's simple enough for us to write our own parser,
    and saves a dependency.

--- factori-impl/src/create.rs
+++ factori-impl/src/create.rs
@@ -1,41 +1,99 @@
-use proc_macro2::TokenStream;
-use proc_macro_rules::rules;
+use proc_macro::TokenStream;
 use quote::quote;
+use syn::parse::{Parse, ParseStream, Result};
+use syn::{parse_macro_input, Expr, Ident, Token};
 
 use super::{ident_builder, ident_mixins_enum};
 
-pub fn create_macro(input: TokenStream) -> TokenStream {
-    rules!(input => {
-        (
-            $ty:ident $(,)?
-            $( : $mixins:ident ),* $(,)?
-            $( $fields:ident: $values:expr ),* $(,)?
-        ) => {
-            let ident_builder = ident_builder(&ty);
-            let ident_mixins_enum = ident_mixins_enum(&ty);
+// e.g. create!(ty, :mixin1, :mixin2, field1: value1, field2: value2)
+//
+// ... becomes:
+//
+// Create {
+//     ty: 'ty',
+//     mixins: vec!['mixin1', 'mixin2'],
+//     fields: vec!['field1', 'field2'],
+//     values: vec!['value1', 'value2'],
+// }
+struct Create {
+    ty: Ident,
+    mixins: Vec<Ident>,
+    fields: Vec<Ident>,
+    values: Vec<Expr>,
+}
 
-            let mut mixins = mixins.iter();
-            let value = if let Some(mixin) = mixins.next() {
-                let initial = quote! {
-                    factori::Mixin::default(#ident_mixins_enum::#mixin)
-                };
-                mixins.fold(initial, |acc, mixin| {
-                    quote! {
-                        factori::Mixin::extend(#ident_mixins_enum::#mixin, #acc)
-                    }
-                })
-            } else {
-                quote! { factori::Default::default () }
-            };
+impl Parse for Create {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let ty = input.parse()?;
 
-            quote! {
-                factori::Builder::build(#ident_builder {
-                    #(
-                        #fields: #values,
-                    )*
-                    .. #value
-                })
+        if input.peek(Token![,]) {
+            input.parse::<Token![,]>()?;
+        }
+
+        let mut mixins = Vec::new();
+        while input.peek(Token![:]) {
+            input.parse::<Token![:]>()?;
+            mixins.push(input.parse()?);
+
+            if input.peek(Token![,]) {
+                input.parse::<Token![,]>()?;
             }
         }
-    })
+
+        let mut fields = Vec::new();
+        let mut values = Vec::new();
+        loop {
+            if input.is_empty() {
+                break;
+            }
+
+            fields.push(input.parse()?);
+            input.parse::<Token![:]>()?;
+            values.push(input.parse()?);
+        }
+
+        Ok(Create {
+            ty,
+            mixins,
+            fields,
+            values,
+        })
+    }
+}
+
+pub fn create_macro(input: TokenStream) -> TokenStream {
+    let Create {
+        ty,
+        mixins,
+        fields,
+        values,
+    } = parse_macro_input!(input);
+
+    let ident_builder = ident_builder(&ty);
+    let ident_mixins_enum = ident_mixins_enum(&ty);
+
+    let mut mixins = mixins.iter();
+    let value = if let Some(mixin) = mixins.next() {
+        let initial = quote! {
+            factori::Mixin::default(#ident_mixins_enum::#mixin)
+        };
+        mixins.fold(initial, |acc, mixin| {
+            quote! {
+                factori::Mixin::extend(#ident_mixins_enum::#mixin, #acc)
+            }
+        })
+    } else {
+        quote! { factori::Default::default () }
+    };
+
+    let quoted = quote! {
+        factori::Builder::build(#ident_builder {
+            #(
+                #fields: #values,
+            )*
+            .. #value
+        })
+    };
+
+    quoted.into()
 }
